7a7410447a797e4feb4257708ca595bab61b65aa
Russ Goetz <russgoetz@gmail.com>
2022-09-06 21:06:10 -0400

    WIP

--- src/mirror.rs
+++ src/mirror.rs
@@ -1,40 +1,52 @@
 use crates_index::Index;
 use std::{collections::HashMap, ops::{Deref, DerefMut}};
-use crate::common::CrateVersion;
 
-struct FeaturesList {
-    list: Vec<String>,
+#[derive(PartialEq, Eq, Hash)]
+struct CrateVersion<'i> {
+    name: &'i str,
+    version: &'i str,
 }
 
-impl FeaturesList {
+impl<'i> CrateVersion<'i> {
+    pub fn new(name: &'i str, version: &'i str) -> Self {
+        CrateVersion { name, version }
+    }
+}
+
+struct FeaturesList<'i> {
+    list: Vec<&'i str>,
+}
+
+impl<'i> FeaturesList<'i> {
     fn new() -> Self {
         FeaturesList { list: Vec::new() }
     }
 
-    fn add_feature(&mut self, feature: &str) {
+    fn add_feature(&mut self, feature: &'i str) {
         if self.list.iter().position(|feat| feat == feature).is_none() {
-            self.list.push(feature.to_string());
+            self.list.push(feature);
         }
     }
 }
 
-impl Deref for FeaturesList {
-    type Target = Vec<String>;
+impl<'i> Deref for FeaturesList<'i> {
+    type Target = Vec<&'i str>;
 
     fn deref(&self) -> &Self::Target {
         &self.list
     }
 }
 
-impl DerefMut for FeaturesList {
+impl DerefMut for FeaturesList<'_> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.list
     }
 }
 
 pub struct CratesIoIndex<'i> {
+    // NOTE: Lifetime of strings is tied to lifetime of Version, which is tied to lifetime of Crate, NOT Index!
     index: &'i Index,
-    crates: HashMap<CrateVersion, FeaturesList>,
+    crates: HashMap<CrateVersion<'i>, FeaturesList<'i>>,
 }
 
 impl<'i> CratesIoIndex<'i> {
